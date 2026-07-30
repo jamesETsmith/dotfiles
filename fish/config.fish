9f0c8150932aa8696a808d0cdfcfbfa1cb713e11
jamesETsmith <james.smith9113@gmail.com>
2026-07-30 20:18:07 +0000

    Adding daily function

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,15 @@ if status is-interactive
   fish_add_path -m $HOME/.local/bin $HOME/.cargo/bin
 end
 
+function daily
+  if test (count $argv) -eq 0
+    echo "Usage: daily <name>" >&2
+    return 2
+  end
+
+  mkdir -- (date +%Y_%m_%d)_(string join _ $argv)
+end
+
 # Put machine-specific or secret values in ~/.config/fish/config.local.fish
 if test -f $HOME/.config/fish/config.local.fish
   source $HOME/.config/fish/config.local.fish
